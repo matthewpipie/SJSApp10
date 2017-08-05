fca408a98ee4d015a6442d9b1fdd0ad065469e3d
matthewpipie <matthewpipie@gmail.com>
2017-08-04 23:20:43 -0500

    firsty

---
+++ SJSApp10/SJSApp10/SJSManager.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+using System.Net;
+
+namespace SJSApp10
+{
+	public class SJSManager
+	{
+		public SJSManager ()
+		{
+		}
+        public void Run()
+        {
+            
+        }
+	}
+}
+
