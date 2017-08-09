9516a4c3435a311e21b5e85d0b74ba8107c1e829
matthewpipie <matthewpipie@gmail.com>
2017-08-08 22:21:29 -0500

    GetAssignments() now accepts dates

--- SJSApp10/SJSApp10/SJSManager.cs
+++ SJSApp10/SJSApp10/SJSManager.cs
@@ -36,9 +36,13 @@ namespace SJSApp10
         {
             LoginManager.SubmitCredentials(username, password, clearToken, callback);
         }
-        public Object GetAssignments(Action<Object> callback)
+        public Object GetAssignments(DateTime start, DateTime end, Action<Object> callback)
         {
-            LoginManager.MakeAPICall("DataDirect/AssignmentCenterAssignments/?format=json&filter=2&dateStart=8%2F5%2F2017&dateEnd=8%2F5%2F2017&persona=2&statusList=&sectionList=", (Object o) => 
+            // %2F is the HTML encoding for a slash (/) which for some really weird reason myschoolapp uses
+            // They also use M-D-YYYY which is just really strange and not standard at all
+            string startDate = start.Month.ToString() + "%2F" + start.Day.ToString() + "%2F" + start.Year.ToString();
+            string endDate = end.Month.ToString() + "%2F" + end.Day.ToString() + "%2F" + end.Year.ToString();
+            LoginManager.MakeAPICall("DataDirect/AssignmentCenterAssignments/?format=json&filter=2&dateStart=" + startDate + "&dateEnd=" + endDate + "&persona=2&statusList=&sectionList=", (Object o) => 
             {
                 if (o != null)
                 {
